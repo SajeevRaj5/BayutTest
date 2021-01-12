5b8f5698d160cc770c9a29b6fe980db00b27b864
Sajeev Raj <sajeevraj.1989@gmail.com>
2021-01-12 21:52:47 +0400

    Initial commit

---
+++ BayutTest/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  BayutTest
+//
+//  Created by sajeev Raj on 12/01/2021.
+//  Copyright © 2021 BayutApp. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
