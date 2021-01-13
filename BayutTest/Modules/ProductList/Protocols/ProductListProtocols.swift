f14003c1a45d5c17a253a076fffadfd32cd7296d
Sajeev Raj <sajeevraj.1989@gmail.com>
2021-01-13 21:45:18 +0400

    Added product list protocol methods

---
+++ BayutTest/Modules/ProductList/Protocols/ProductListProtocols.swift
@@ -0,0 +1,44 @@
+//
+//  ProductListProtocols.swift
+//  BayutTest
+//
+//  Created by sajeev Raj on 13/01/2021.
+//  Copyright © 2021 BayutApp. All rights reserved.
+//
+
+import Foundation
+
+// protocol to handle interaction from view to presenter
+protocol ViewToPresenterProductListProtocol {
+    var view: PresenterToViewProductListProtocol? {get set}
+    var interactor: PresenterToInteractorProductListProtocol? {get set}
+    var router: PresenterToRouterProtocol? {get set}
+    var products: [Product]? {get set}
+    
+    func fetchAllProducts()
+}
+
+// protocol to handle interaction from presenter to view
+protocol PresenterToViewProductListProtocol: class {
+    func showProducts(products: [ProductViewModel])
+    func showError(error: Error)
+    func dismissLoader()
+}
+
+// protocol to handle interaction from presenter to interactor
+protocol PresenterToInteractorProductListProtocol {
+    var presenter: InteractorToPresenterProductListProtocol? {get set}
+    func fetchProducts()
+}
+
+// protocol to handle interaction from interactor to presenter
+protocol InteractorToPresenterProductListProtocol: class {
+    func onSuccessProductsFetch(products: [Product])
+    func onFailedProductsFetch(error: Error)
+    func onServerResponseReceival()
+}
+
+// protocol to handle navigation
+protocol PresenterToRouterProtocol {
+    static func createModule() -> ProductListViewController
+}
---
+++ BayutTest/Modules/ProductList/View/ProductListPresenter.swift
@@ -0,0 +1,9 @@
+//
+//  ProductListPresenter.swift
+//  BayutTest
+//
+//  Created by sajeev Raj on 13/01/2021.
+//  Copyright © 2021 BayutApp. All rights reserved.
+//
+
+import Foundation
