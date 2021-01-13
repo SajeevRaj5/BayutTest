84941fdfc53a50254868ba413d202add78613a9f
Sajeev Raj <sajeevraj.1989@gmail.com>
2021-01-13 21:27:52 +0400

    Created model class

---
+++ BayutTest/Modules/ProductList/Entity/Product.swift
@@ -0,0 +1,52 @@
+//
+//  Product.swift
+//  BayutTest
+//
+//  Created by sajeev Raj on 12/01/2021.
+//  Copyright © 2021 BayutApp. All rights reserved.
+//
+
+import Foundation
+
+class Product: Codable {
+    let createdAt, price, name, uid: String
+    let imageIDS: [String]
+    let imageUrls, imageUrlsThumbnails: [String]
+
+    enum CodingKeys: String, CodingKey {
+        case createdAt = "created_at"
+        case price, name, uid
+        case imageIDS = "image_ids"
+        case imageUrls = "image_urls"
+        case imageUrlsThumbnails = "image_urls_thumbnails"
+    }
+    
+    static func getProducts(completion: @escaping (ServiceResponse<ProductResponse>) -> ()) {
+        Router.products.request { (response: ServiceResponse<ProductResponse>) in
+             switch response {
+             case .success(let results):
+                 completion(.success(data: results))
+             case .failure(let error):
+                 completion(.failure(error: error))
+             case .finally:
+                 completion(.finally)
+             }
+         }
+    }
+}
+
+extension Product {
+    enum Router: Requestable {
+        case products
+        
+        var path: String {
+            switch self {
+            case .products: return "dynamodb-writer"
+            }
+        }
+    }
+}
+
+class ProductResponse: Codable {
+    let products: [Product]? = nil
+}
