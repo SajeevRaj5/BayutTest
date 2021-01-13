39d11432b7639634899ae302bc528e025116331b
Sajeev Raj <sajeevraj.1989@gmail.com>
2021-01-13 23:21:14 +0400

    Defined protocol methods in Presenter and Interactor classes

--- BayutTest/Modules/ProductList/Interactor/ProductListInteractor.swift
+++ BayutTest/Modules/ProductList/Interactor/ProductListInteractor.swift
@@ -7,3 +7,28 @@
 //
 
 import Foundation
+
+class ProductListInteractor: PresenterToInteractorProductListProtocol {
+    weak var presenter: InteractorToPresenterProductListProtocol?
+    
+    func fetchProducts() {
+        Product.getProducts { [weak self] (response) in
+            guard let welf = self else { return }
+            
+            switch response {
+            case .success(let response):
+                guard let products = response.results else { return }
+                
+                // send events to presenter
+                welf.presenter?.onSuccessProductsFetch(products: products)
+                
+            case .failure(let error):
+                welf.presenter?.onFailedProductsFetch(error: error)
+            case .finally:
+                welf.presenter?.onServerResponseReceival()
+            }
+        }
+    }
+    
+    
+}
--- BayutTest/Modules/ProductList/Presenter/ProductListPresenter.swift
+++ BayutTest/Modules/ProductList/Presenter/ProductListPresenter.swift
@@ -7,3 +7,32 @@
 //
 
 import Foundation
+
+class ProductListPresenter: ViewToPresenterProductListProtocol {
+    
+    var products: [Product]?
+    
+    weak var view: PresenterToViewProductListProtocol?
+    
+    var interactor: PresenterToInteractorProductListProtocol?
+    
+    var router: PresenterToRouterProtocol?
+    
+    func fetchAllProducts() {
+        interactor?.fetchProducts()
+    }
+}
+
+extension ProductListPresenter: InteractorToPresenterProductListProtocol {
+    func onSuccessProductsFetch(products: [Product]) {
+        
+    }
+    
+    func onFailedProductsFetch(error: Error) {
+        
+    }
+    
+    func onServerResponseReceival() {
+        
+    }
+}
